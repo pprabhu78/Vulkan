577374c04cc543cb39f12e8f27ab80c681a90a9b
pprabhu78 <poojanprabhu@gmail.com>
2023-02-08 11:50:11 -0500

    -doc, init variables

--- genesis/StorageImage.h
+++ genesis/StorageImage.h
@@ -14,21 +14,31 @@ namespace genesis
 {
    class Device;
 
-   //! generic storage image
+   //! An image directly made with flags for usage, memory type, tiling and sample count
+   //! It is used for storing results of some intermediate rendering (e.g. ray tracing), blit screenshots, etc
    class StorageImage : public Image
    {
    public:
+      //! constructor
       StorageImage(Device* device, VkFormat format, int width, int height
          , VkImageUsageFlags usageFlags, VkMemoryPropertyFlags memoryPropertyFlags, VkImageTiling imageTiling, int sampleCount);
 
+      //! destructor
       virtual ~StorageImage();
+   private:
+      //! not allowed
+      StorageImage() = delete;
+      //! not allowed
+      StorageImage(const StorageImage& rhs) = delete;
+      //! not allowed
+      StorageImage& operator=(const StorageImage& rhs) = delete;
    public:
       //! get the view. this is lazily created
       virtual const VkImageView& vulkanImageView(void) const;
    protected:
 
-      VkImageView _imageView;
-      const VkImageUsageFlags _usageFlags;
+      VkImageView _imageView = VK_NULL_HANDLE;
+      const VkImageUsageFlags _usageFlags = 0;
    };
 }
 
