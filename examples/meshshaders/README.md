d032a47efc874f8f9fa9ee1cd9d69516648c1deb
pprabhu78 <poojanprabhu@gmail.com>
2023-03-26 18:01:14 -0400

    -mesh shaders

---
+++ examples/meshshaders/README.md
@@ -0,0 +1,13 @@
+# Mesh Shaders
+
+![enter image description here](https://github.com/pprabhu78/Vulkan/blob/master/screenshots/mesh_shaders.png)
+
+This sample is based off of the DirectX 12 Mesh Shader Samples:
+
+https://learn.microsoft.com/en-us/samples/microsoft/directx-graphics-samples/d3d12-mesh-shader-samples-win32/
+
+-An obj with face normals is used as an input to the first example there: Wavefront Converter Command Line Tool
+-This mesh is then loaded by VulkanMeshlet
+-It is then rendered using mesh shaders
+-The implementation is the Vulkan equivalent of the 'Meshlet Viewer' sample from the above link
+
--- examples/meshshaders/meshshaders.cpp
+++ examples/meshshaders/meshshaders.cpp
@@ -6,6 +6,13 @@
 * This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
 */
 
+//! This sample is based off of the DirectX 12 Mesh Shader Samples:
+//! https://learn.microsoft.com/en-us/samples/microsoft/directx-graphics-samples/d3d12-mesh-shader-samples-win32/
+//! -An obj with face normals is used as an input to the first example there: Wavefront Converter Command Line Tool
+//! -This mesh is then loaded by VulkanMeshlet
+//! -It is then rendered using mesh shaders
+//! -The implementation is the Vulkan equivalent of the 'Meshlet Viewer' sample from the above link
+
 #include "meshshaders.h"
 
 #include "Device.h"
